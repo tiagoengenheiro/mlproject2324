4b2e2c94e67d7f185f50b21ceba1187ec68587b1
tae <tiagoengenheiro.11@gmail.com>
2023-09-21 16:50:27 +0100

    centered data + final predictions

--- exercise1/exercise1.py
+++ exercise1/exercise1.py
@@ -7,13 +7,17 @@ Created on Thu Sep 14 15:37:29 2023
 
 import numpy as np
 import random 
-from sklearn.linear_model import LinearRegression,Lasso,Ridge,RidgeCV,LassoCV
+from sklearn.linear_model import LinearRegression,Lasso,Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
-from sklearn.model_selection import cross_val_score,KFold
+from sklearn.model_selection import cross_validate
 
 x_train=np.load("X_train_regression1.npy") #shape -> (15,10)
 y_train=np.load("y_train_regression1.npy") #shape -> (15,1)
+
+print(y_train.shape)
+## CENTERING INPUT DATA
+x_train=x_train - np.mean(x_train,axis=0) #axis=0 -> por coluna
 
 
 n_examples,n_features=x_train.shape
@@ -22,19 +26,22 @@ column_of_ones=np.ones((n_examples,1)) #shape (15,1)
 X_train=np.hstack((column_of_ones,x_train)) #shape -> (15,11)
 
 
-Beta=np.dot(np.dot(np.linalg.inv(np.dot(X_train.T,X_train)),X_train.T),y_train) # (11,1)
-
-y_pred=np.dot(X_train,Beta)
-
-
+#Beta=np.dot(np.dot(np.linalg.inv(np.dot(X_train.T,X_train)),X_train.T),y_train) # (11,1)
+#y_pred=np.dot(X_train,Beta)
 
 regr = LinearRegression()
 
 regr.fit(X_train,y_train)
 
 
+scores=(-1)*cross_validate(
+    regr, 
+    X_train, 
+    y_train, 
+    cv=5, 
+    scoring=('r2', 'neg_mean_squared_error'), return_train_score=True
+    )["test_neg_mean_squared_error"]
 
-scores=-1*cross_val_score(regr, X_train,y_train,cv=5,scoring="neg_mean_squared_error")
 alphas = [0.01, 0.1, 1, 10, 100, 1000]
 
 best_mean_score=np.mean(scores)
@@ -43,15 +50,14 @@ best_alpha=0
 
 print(best_alpha,best_method,best_mean_score,np.std(scores))
 
-
 for alpha in alphas:
     for method in ["ridge","lasso"]:
         if method=="ridge":
             model=Ridge(alpha)
         else:
             model=Lasso(alpha)
-        model.fit(x_train,y_train)
-        scores=-1*cross_val_score(model,x_train,y_train,cv=5,scoring="neg_mean_squared_error")
+        model.fit(X_train,y_train)
+        scores=(-1)*cross_validate(model, X_train, y_train, cv=5, scoring=('r2', 'neg_mean_squared_error'))["test_neg_mean_squared_error"]
         score_mean=np.mean(scores)
         score_std=np.std(scores)
         print(alpha,method,score_mean,score_std)
@@ -66,22 +72,15 @@ print("the best method is %s regression with an alpha of %s and a mean score of 
     best_alpha,
     best_mean_score))
 
-# def crosskvalidation(k,X_train,y_train):
-#     guideArray=[i for i in range(0,X_train.shape[0],X_train.shape[0]//k)]+[X_train.shape[0]]
-#     meanError=[]
-#     for el1,el2 in zip(guideArray[:-1],guideArray[1:]):
-#         print(el1,el2)
-#         X_train_k=np.vstack((X_train[:el1],X_train[el2:]))
-#         y_train_k=np.vstack((y_train[:el1],y_train[el2:]))
-#         regr.fit(X_train_k,y_train_k)
-#         y_pred=regr.predict(X_train[el1:el2])
-#         print(mean_squared_error(y_train[el1:el2], y_pred))
-#         meanError.append(mean_squared_error(y_train[el1:el2], y_pred))
-#     return meanError
 
-#print(np.mean(crosskvalidation(5, X_train, y_train)))
+#Computing the final predictions
+model=Lasso(alpha=0.1) 
+model.fit(X_train,y_train)
+x_test=np.load("X_test_regression1.npy") #shape -> (15,10)
+n_examples,_=x_test.shape
+column_of_ones=np.ones((n_examples,1)) #shape (15,1)
+X_test=np.hstack((column_of_ones,x_test)) #shape -> (15,11)
+preds=model.predict(X_test).reshape(n_examples,1)
 
-        
-#TODO: why is Lasso better, indica mesmo que há variáveis irrelevantes para o outcome. Penalização suave, apenas com 0.1
     
     
