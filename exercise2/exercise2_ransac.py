86cc4f664d5974daf7fc018a0b0ab9e8e68e1a2f
tae <tiagoengenheiro.11@gmail.com>
2023-10-07 16:12:41 +0100

    add .gitignore

--- exercise2/exercise2_ransac.py
+++ exercise2/exercise2_ransac.py
@@ -16,12 +16,12 @@ MAE=np.mean(np.abs(y_train_init-np.mean(y_train_init)))
 min_mse=(np.inf,np.inf)
 model1,model2=None,None
 best_n_samples=0
-th=0
-for threshold in [MAD]:
+best_th=0
+for threshold in [MAD,MAE]:
     print("Threshold:", threshold)
     print("How many values above threshold:",len(y_train_init[y_train_init>threshold]))
     max_n_samples=len(y_train_init[y_train_init>threshold])
-    for n_samples in range(1,10,1):
+    for n_samples in range(1,40,1):
         reg = RANSACRegressor(random_state=0,min_samples=n_samples,max_trials=200,residual_threshold=threshold,loss='absolute_error',stop_probability=1.0)
         reg.fit(X_train_init,y_train_init)
         #print("Number of inliers",len(reg.inlier_mask_[reg.inlier_mask_==True]))
@@ -31,18 +31,19 @@ for threshold in [MAD]:
         if (mse1+mse2) < sum(min_mse):
             min_mse=(mse1,mse2)
             best_n_samples=n_samples
-            th=threshold
-    print("Best MS1",min_mse[0],"Best MS2",min_mse[1])
+            best_th=threshold
+    print("Best MS1:",min_mse[0],"Best MS2:",min_mse[1])
     print("\n")
-print("Best Threshold",threshold)
-print("Best number of samples",best_n_samples)
+print("Best Threshold:",best_th)
+print("Best number of samples:",best_n_samples)
 print(model1,min_mse[0])
 print(model2,min_mse[1])
 
-reg = RANSACRegressor(random_state=0,min_samples=best_n_samples,residual_threshold=threshold,loss='absolute_error')
+reg = RANSACRegressor(random_state=0,min_samples=best_n_samples,residual_threshold=best_th,loss='absolute_error')
 reg.fit(X_train_init,y_train_init)
-print("Number of inliers",len(reg.inlier_mask_[reg.inlier_mask_==True]))
-print("List of Outliers indexes",[i for i,x in enumerate(reg.inlier_mask_) if x==False])
+print("Number of inliers:",len(reg.inlier_mask_[reg.inlier_mask_==True]))
+print("List of Outliers indexes:",[i for i,x in enumerate(reg.inlier_mask_) if x==False])
+
 
 model1,mse1=get_best_model(X_train_init[reg.inlier_mask_],y_train_init[reg.inlier_mask_])
 model2,mse2=get_best_model(X_train_init[~reg.inlier_mask_],y_train_init[~reg.inlier_mask_])
@@ -51,7 +52,7 @@ results=np.hstack((model1.predict(X_train_init),model2.predict(X_train_init).res
 results = np.square(results-y_train_init)
 results = np.min(results,axis=1)
 results = np.sum(results,axis=0)
-print("SSE:",results)
+print("SSE in X_train:",results)
 
 
 X_test_init=np.load("X_test_regression2.npy")
