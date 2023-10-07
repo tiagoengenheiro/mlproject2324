50bc776a60fcc159cb56618eccaab86eb1ce7c35
tae <tiagoengenheiro.11@gmail.com>
2023-10-07 15:26:34 +0100

    add y_test

--- exercise2/exercise2_ransac.py
+++ exercise2/exercise2_ransac.py
@@ -8,15 +8,9 @@ from sklearn.preprocessing import RobustScaler
 from utils import get_best_model
 X_train_init=np.load("X_train_regression2.npy") #shape -> (100, 4)
 y_train_init=np.load("y_train_regression2.npy") #shape -> (100, 1)
-X_test_init=np.load("X_test_regression2.npy")
-print(X_test_init.shape)
+
 n_examples,n_features=X_train_init.shape
 
-# RS = RobustScaler(with_centering=True, with_scaling=False) ##RS = StandardScaler(with_centering=True, with_scaling=False)
-# Outliers
-# X_train = RS.fit_transform(X_train_init)
-# y_train = RS.fit_transform(y_train_init)
-# MAD=np.median(np.abs(y_train_init-np.median(y_train_init)))
 MAD=np.median(np.abs(y_train_init-np.median(y_train_init))) #0.9395742394919429
 MAE=np.mean(np.abs(y_train_init-np.mean(y_train_init)))
 min_mse=(np.inf,np.inf)
@@ -30,7 +24,7 @@ for threshold in [MAD]:
     for n_samples in range(1,10,1):
         reg = RANSACRegressor(random_state=0,min_samples=n_samples,max_trials=200,residual_threshold=threshold,loss='absolute_error',stop_probability=1.0)
         reg.fit(X_train_init,y_train_init)
-    # print("Number of inliers",len(reg.inlier_mask_[reg.inlier_mask_==True]))
+        #print("Number of inliers",len(reg.inlier_mask_[reg.inlier_mask_==True]))
         model1,mse1=get_best_model(X_train_init[reg.inlier_mask_],y_train_init[reg.inlier_mask_])
         model2,mse2=get_best_model(X_train_init[~reg.inlier_mask_],y_train_init[~reg.inlier_mask_])
         print(n_samples,"MSE1",mse1,"MSE2",mse2)
@@ -49,8 +43,6 @@ reg = RANSACRegressor(random_state=0,min_samples=best_n_samples,residual_thresho
 reg.fit(X_train_init,y_train_init)
 print("Number of inliers",len(reg.inlier_mask_[reg.inlier_mask_==True]))
 print("List of Outliers indexes",[i for i,x in enumerate(reg.inlier_mask_) if x==False])
-print(reg.n_skips_no_inliers_)
-print(reg.n_trials_)
 
 model1,mse1=get_best_model(X_train_init[reg.inlier_mask_],y_train_init[reg.inlier_mask_])
 model2,mse2=get_best_model(X_train_init[~reg.inlier_mask_],y_train_init[~reg.inlier_mask_])
@@ -60,3 +52,17 @@ results = np.square(results-y_train_init)
 results = np.min(results,axis=1)
 results = np.sum(results,axis=0)
 print("SSE:",results)
+
+
+X_test_init=np.load("X_test_regression2.npy")
+print(X_test_init.shape)
+
+n_examples,n_features=X_test_init.shape
+
+y_pred_1=model1.predict(X_test_init).reshape(n_examples,1)
+y_pred_2=model2.predict(X_test_init).reshape(n_examples,1)
+y_pred=np.hstack((y_pred_1,y_pred_2))
+print(y_pred.shape)
+np.save("y_test_regression2.npy",y_pred)
+y_pred=np.load("y_test_regression2.npy")
+print(y_pred.shape)
