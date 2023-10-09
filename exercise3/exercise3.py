c1105622e9558387d16b70071832e5cde3e65dc2
tae <tiagoengenheiro.11@gmail.com>
2023-10-09 10:58:03 +0100

    update exercise3

--- exercise3/exercise3.py
+++ exercise3/exercise3.py
@@ -1,11 +1,29 @@
 
 import numpy as np
 from utils import get_best_model
-X_train=np.load("Xtrain_Classification1.npy")
-y_train=np.load("ytrain_Classification1.npy")
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import SGD
+from sklearn.model_selection import train_test_split
+X=np.load("Xtrain_Classification1.npy")
+y=np.load("ytrain_Classification1.npy")
 
-print(X_train.shape)
-print(y_train.shape) 
-print(sum(y_train)) #896 are melanoma and and the rest (5000) is nevu. nevu>>melanoma
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1) #0.8 - 0.2 
 
-get_best_model(X_train,y_train)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1) # 0.25 x 0.8 = 0.2
+#0.6 Train e 0.2 Val e 0.2 Test
+
+n_examples_train,n_features=X_train.shape
+n_examples_test,_=X_test.shape
+n_examples_val,_=X_val.shape
+print(n_examples_train,n_examples_val,n_examples_test)
+model = Sequential()
+model.add(Dense(256, input_shape=(n_features,), activation="sigmoid"))
+model.add(Dense(128, activation="sigmoid"))
+model.add(Dense(2, activation="softmax"))
+
+# sgd = SGD(0.01)
+# model.compile(loss="categorical_crossentropy", optimizer=sgd,
+# 	metrics=["accuracy"])
+# H = model.fit(X_train, y_train, validation_data=(testX, testY),
+# 	epochs=100, batch_size=128)
