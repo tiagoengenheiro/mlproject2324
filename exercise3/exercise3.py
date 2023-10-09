69da878133e3743bc2b942597e1cac96adcf95d2
tae <tiagoengenheiro.11@gmail.com>
2023-10-09 11:30:31 +0100

    add predictions

--- exercise3/exercise3.py
+++ exercise3/exercise3.py
@@ -6,7 +6,7 @@ from keras.layers import Dense
 from keras.optimizers import SGD
 from sklearn.model_selection import train_test_split
 X=np.load("Xtrain_Classification1.npy")
-y=np.load("ytrain_Classification1.npy")
+y=np.load("ytrain_Classification1.npy").reshape(X.shape[0],1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1) #0.8 - 0.2 
 
@@ -20,10 +20,12 @@ print(n_examples_train,n_examples_val,n_examples_test)
 model = Sequential()
 model.add(Dense(256, input_shape=(n_features,), activation="sigmoid"))
 model.add(Dense(128, activation="sigmoid"))
-model.add(Dense(2, activation="softmax"))
+model.add(Dense(1, activation="sigmoid"))
+print(model.output_shape)
+sgd = SGD(0.01)
+model.compile(loss="binary_crossentropy", optimizer=sgd,metrics=["accuracy"])
+model.fit(X_train, y_train, validation_data=(X_val, y_val),
+	epochs=100, batch_size=128)
 
-# sgd = SGD(0.01)
-# model.compile(loss="categorical_crossentropy", optimizer=sgd,
-# 	metrics=["accuracy"])
-# H = model.fit(X_train, y_train, validation_data=(testX, testY),
-# 	epochs=100, batch_size=128)
+predictions = model.predict(X_test, batch_size=128)
+print(predictions)
