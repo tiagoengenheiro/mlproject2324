a6dca1178e3b4430428c9de4bf664bc344dacbbf
tae <tiagoengenheiro.11@gmail.com>
2023-10-23 16:33:52 +0100

    add multi class augmentation

---
+++ exercise4/sklearn_models.py
@@ -0,0 +1,45 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import balanced_accuracy_score
+from augmentations import *
+
+
+X=np.load("Xtrain_Classification2.npy")
+y=np.load("ytrain_Classification2.npy")
+#X_test=np.load("Xtest_Classification2.npy")
+
+
+random_state=42
+#No augmentation
+cv=StratifiedKFold(n_splits=5,shuffle=True,random_state=random_state)
+
+rf_classifier = RandomForestClassifier(n_estimators=100,criterion = 'entropy', random_state=random_state)
+svm_classifier = SVC(kernel='rbf', C=1, gamma='scale',random_state=random_state)
+gaussian_nb  = GaussianNB()
+
+clfs={
+    "RF":[],
+    "SVM":[],
+    "GNB":[],
+}
+for i, (train_index, test_index) in enumerate(cv.split(X, y)):
+    X_train,y_train=X[train_index],y[train_index]
+    X_train,y_train=self_augmentation(X_train,y_train)
+    X_test,y_test=X[test_index],y[test_index]
+    for i,clf in enumerate([rf_classifier,svm_classifier,gaussian_nb]):
+        clf.fit(X_train,y_train)
+        pred=clf.predict(X_test)
+        b_acc=balanced_accuracy_score(y_test,pred)
+        keys=list(clfs.keys())
+        clfs[keys[i]].append(b_acc)
+        # print([keys[i]],b_acc)
+
+for key in clfs:
+    print(f"{key} mean: {np.mean(clfs[key])}  std: {np.std(clfs[key])}")
+
--- exercise4/visualizing_data.py
+++ exercise4/visualizing_data.py
@@ -1,25 +1,27 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-
+import numpy as np
 X=np.load("Xtrain_Classification2.npy")
 y=np.load("ytrain_Classification2.npy")
+X_test=np.load("Xtest_Classification2.npy")
 
-print(X.shape)
+X=X.reshape(X.shape[0],28,28,3)
 
-# choices=sorted(np.arange(0,X.shape[0],1))
-# print(y[0])
-# num_of_samples=50
-# indexes=random.sample(choices,num_of_samples)
+print(np.unique(y),"\n",np.round(100*(np.array(np.bincount(np.array(y,dtype=np.int64)),dtype=np.float32)/X.shape[0]),1))
+choices=sorted(np.arange(0,X.shape[0],1))
+num_of_samples=100
 
-# fig, axs = plt.subplots(nrows=5, ncols=10, figsize=(14, 7),
-#                         subplot_kw={'xticks': [], 'yticks': []})
-
-
-# for ax, ind in zip(axs.flat, indexes):
-#     ax.imshow(X[ind])
-#     ax.set_title(f"Classified as {int(y[ind])}")
+indexes=random.sample(choices,num_of_samples)
+print(indexes)
+fig, axs = plt.subplots(nrows=10, ncols=10, figsize=(20, 14),
+                        subplot_kw={'xticks': [], 'yticks': []})
 
 
-# plt.tight_layout()
-# plt.savefig(f"images/{num_of_samples}SamplesVisualization.png")
+for ax, ind in zip(axs.flat, indexes):
+    ax.imshow(X[ind])
+    ax.set_title(f"Classified as {int(y[ind])}")
+
+
+plt.tight_layout()
+plt.savefig(f"images/{num_of_samples}SamplesVisualization.png")
